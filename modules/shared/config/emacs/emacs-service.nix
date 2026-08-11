9722a05e4e327c5920f143ea2b1910504014c142
Alán F. Muñoz <afer.mg@gmail.com>
2026-08-11 18:02:27 -0400

    fix(emacs): install client binaries in home profile

--- modules/shared/config/emacs/emacs-service.nix
+++ modules/shared/config/emacs/emacs-service.nix
@@ -1,13 +1,22 @@
 { pkgs, ... }:
+let
+  emacsPackage = pkgs.emacs.override {
+    withImageMagick = true;
+    withXwidgets = false; # https://github.com/nix-community/emacs-overlay/issues/466
+  };
+in
 {
+  # `services.emacs.package` controls the daemon executable but does not add
+  # Emacs or emacsclient to the Home Manager profile. Enable the program too
+  # so both commands remain available on PATH.
+  programs.emacs = {
+    enable = true;
+    package = emacsPackage;
+  };
+
   services.emacs = {
     enable = true;
     startWithUserSession = "graphical";
-    package =
-      (pkgs.emacs.override {
-        withImageMagick = true;
-        withXwidgets = false; # https://github.com/nix-community/emacs-overlay/issues/466
-      }).pkgs.withPackages
-        (_epkgs: [ ]);
+    package = emacsPackage.pkgs.withPackages (_epkgs: [ ]);
   };
 }
