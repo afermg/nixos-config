6f7ded6a55b747c01e2cd37d7ced9902d1519dfb
Alán F. Muñoz <afer.mg@gmail.com>
2026-09-18 17:03:31 -0400

    chore(nix): remove CUDA maintainer cache
    Remove the CUDA maintainer substituter and its matching trusted public key.

--- machines/common/substituters.nix
+++ machines/common/substituters.nix
@@ -3,13 +3,11 @@
   nix.settings = {
     substituters = [
       "https://devenv.cachix.org"
-      "https://cuda-maintainers.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://ai.cachix.org"
     ];
     trusted-public-keys = [
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
     ];
